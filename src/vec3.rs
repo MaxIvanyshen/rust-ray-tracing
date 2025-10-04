b9508325fd5dfc8ee4ea894ecaf0f648795f8fb3
MaxIvanyshen <maxivanyshen@gmail.com>
2025-10-04 16:11:25 +0300

    vec3 definitions

---
+++ src/vec3.rs
@@ -0,0 +1,121 @@
+use std::fmt;
+use std::ops::{Add, Sub, Mul, Div};
+
+
+pub struct Vec3 {
+    pub e: [f32; 3],
+}
+
+pub fn new(e0: f32, e1: f32, e2: f32) -> Vec3 {
+    Vec3{
+        e: [e0, e1, e2]
+    }
+}
+
+impl Vec3 {
+    pub fn x(&self) -> f32 {
+        self.e[0]
+    }
+    pub fn y(&self) -> f32 {
+        self.e[1]
+    }
+    pub fn z(&self) -> f32 {
+        self.e[2]
+    }
+
+    pub fn dot(&self, other: &Vec3) -> f32 {
+        self.e[0] * other.e[0] +
+        self.e[1] * other.e[1] +
+        self.e[2] * other.e[2]
+    }
+
+    pub fn cross(&self, other: &Vec3) -> Vec3 {
+        Vec3{
+            e: [
+                self.e[1] * other.e[2] - self.e[2] * other.e[1],
+                self.e[2] * other.e[0] - self.e[0] * other.e[2],
+                self.e[0] * other.e[1] - self.e[1] * other.e[0],
+            ]
+        }
+    } 
+} 
+
+impl fmt::Display for Vec3 {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "{} {} {}", self.e[0], self.e[1], self.e[2])
+    }
+}
+
+impl Add for Vec3 {
+    type Output = Vec3;
+
+    fn add(self, other: Vec3) -> Self::Output {
+        Vec3{
+            e: [
+                self.e[0] + other.e[0],
+                self.e[1] + other.e[1],
+                self.e[2] + other.e[2],
+            ] 
+        }
+    }
+}
+
+impl Sub for Vec3 {
+    type Output = Vec3;
+
+    fn sub(self, other: Vec3) -> Self::Output {
+        Vec3{
+            e: [
+                self.e[0] - other.e[0],
+                self.e[1] - other.e[1],
+                self.e[2] - other.e[2],
+            ] 
+        }
+    }
+}
+
+impl Mul for Vec3 {
+    type Output = Vec3;
+
+    fn mul(self, other: Vec3) -> Self::Output {
+        Vec3{
+            e: [
+                self.e[0] * other.e[0],
+                self.e[1] * other.e[1],
+                self.e[2] * other.e[2],
+            ] 
+        }
+    }
+
+}
+
+impl Mul<f32> for Vec3 {
+    type Output = Vec3;
+
+    fn mul(self, rhs: f32) -> Vec3 {
+        Vec3 {
+            e: [
+                self.e[0] * rhs,
+                self.e[1] * rhs,
+                self.e[2] * rhs,
+            ],
+        }
+    }
+}
+
+impl Mul<Vec3> for f32 {
+    type Output = Vec3;
+
+    fn mul(self, rhs: Vec3) -> Vec3 {
+        rhs * self
+    }
+}
+
+impl Div<f32> for Vec3 {
+    type Output = Vec3;
+
+    fn div(self, rhs: f32) -> Vec3 {
+        1.0 / rhs * self
+    }
+}
+
