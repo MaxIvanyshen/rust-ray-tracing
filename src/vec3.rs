197077ec0b7ee60111f9dd4d1150cebd2e9c404b
MaxIvanyshen <maxivanyshen@gmail.com>
2025-10-05 03:06:16 +0300

    fix: full vec3 definition

--- src/vec3.rs
+++ src/vec3.rs
@@ -1,16 +1,20 @@
+use std::ops::{Add, Sub, Mul, Div, Neg, Index, IndexMut, AddAssign, MulAssign, DivAssign};
 use std::fmt;
-use std::ops::{Add, Sub, Mul, Div};
 
-
-pub struct Point3 {
+#[derive(Clone, Copy)]
+pub struct Vec3 {
     e: [f32; 3],
 }
 
-pub type Vec3 = Point3;
+impl fmt::Debug for Vec3 {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "Vec3({}, {}, {})", self.e[0], self.e[1], self.e[2])
+    }
+}
 
-pub fn new(e1: f32, e2: f32, e3: f32) -> Vec3 {
-    Vec3{
-        e: [e1, e2, e3],
+impl fmt::Display for Vec3 {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "{} {} {}", self.e[0], self.e[1], self.e[2])
     }
 }
 
@@ -25,26 +29,30 @@ impl Vec3 {
         self.e[2]
     }
 
+    pub fn length_squared(&self) -> f32 {
+        self.e[0] * self.e[0] + self.e[1] * self.e[1] + self.e[2] * self.e[2]
+    }
+
+    pub fn length(&self) -> f32 {
+        self.length_squared().sqrt()
+    }
+
     pub fn dot(&self, other: &Vec3) -> f32 {
-        self.e[0] * other.e[0] +
-        self.e[1] * other.e[1] +
-        self.e[2] * other.e[2]
+        self.e[0] * other.e[0] + self.e[1] * other.e[1] + self.e[2] * other.e[2]
     }
 
     pub fn cross(&self, other: &Vec3) -> Vec3 {
-        Vec3{
+        Vec3 {
             e: [
                 self.e[1] * other.e[2] - self.e[2] * other.e[1],
                 self.e[2] * other.e[0] - self.e[0] * other.e[2],
                 self.e[0] * other.e[1] - self.e[1] * other.e[0],
-            ]
+            ],
         }
-    } 
-} 
+    }
 
-impl fmt::Display for Vec3 {
-    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        write!(f, "{} {} {}", self.e[0], self.e[1], self.e[2])
+    pub fn unit_vector(&self) -> Vec3 {
+        *self / self.length()
     }
 }
 
@@ -52,12 +60,12 @@ impl Add for Vec3 {
     type Output = Vec3;
 
     fn add(self, other: Vec3) -> Self::Output {
-        Vec3{
+        Vec3 {
             e: [
                 self.e[0] + other.e[0],
                 self.e[1] + other.e[1],
                 self.e[2] + other.e[2],
-            ] 
+            ],
         }
     }
 }
@@ -66,29 +74,14 @@ impl Sub for Vec3 {
     type Output = Vec3;
 
     fn sub(self, other: Vec3) -> Self::Output {
-        Vec3{
+        Vec3 {
             e: [
                 self.e[0] - other.e[0],
                 self.e[1] - other.e[1],
                 self.e[2] - other.e[2],
-            ] 
+            ],
         }
     }
-}
-
-impl Mul for Vec3 {
-    type Output = Vec3;
-
-    fn mul(self, other: Vec3) -> Self::Output {
-        Vec3{
-            e: [
-                self.e[0] * other.e[0],
-                self.e[1] * other.e[1],
-                self.e[2] * other.e[2],
-            ] 
-        }
-    }
-
 }
 
 impl Mul<f32> for Vec3 {
@@ -121,3 +114,74 @@ impl Div<f32> for Vec3 {
     }
 }
 
+impl Neg for Vec3 {
+    type Output = Vec3;
+
+    fn neg(self) -> Self::Output {
+        Vec3 { e: [-self.e[0], -self.e[1], -self.e[2]] }
+    }
+}
+
+impl Index<usize> for Vec3 {
+    type Output = f32;
+
+    fn index(&self, index: usize) -> &Self::Output {
+        &self.e[index]
+    }
+}
+
+impl IndexMut<usize> for Vec3 {
+    fn index_mut(&mut self, index: usize) -> &mut Self::Output {
+        &mut self.e[index]
+    }
+}
+
+impl AddAssign for Vec3 {
+    fn add_assign(&mut self, other: Vec3) {
+        self.e[0] += other.e[0];
+        self.e[1] += other.e[1];
+        self.e[2] += other.e[2];
+    }
+}
+
+impl MulAssign<f32> for Vec3 {
+    fn mul_assign(&mut self, rhs: f32) {
+        self.e[0] *= rhs;
+        self.e[1] *= rhs;
+        self.e[2] *= rhs;
+    }
+}
+
+impl DivAssign<f32> for Vec3 {
+    fn div_assign(&mut self, rhs: f32) {
+        *self *= 1.0 / rhs;
+    }
+}
+
+impl std::cmp::PartialEq for Vec3 {
+    fn eq(&self, other: &Self) -> bool {
+        self.e[0] == other.e[0] &&
+        self.e[1] == other.e[1] &&
+        self.e[2] == other.e[2]
+    }
+}
+
+impl std::default::Default for Vec3 {
+    fn default() -> Self {
+        Vec3 { e: [0.0, 0.0, 0.0] }
+    }
+}
+
+pub type Point3 = Vec3;
+
+pub fn new(e1: f32, e2: f32, e3: f32) -> Vec3 {
+    Vec3 {
+        e: [e1, e2, e3],
+    }
+}
+
+pub fn new_point(e1: f32, e2: f32, e3: f32) -> Point3 {
+    Point3 {
+        e: [e1, e2, e3],
+    }
+}
